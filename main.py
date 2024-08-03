78dd0b4d8534d7a47922cf1317ca73a51ce53c2f
Nikita Lazarev <Ni.Lazarev@ase-ec.ru>
2024-08-03 14:49:17 +0300

    error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 from dotenv import load_dotenv
-
+import requests
 
 load_dotenv()
 
@@ -29,6 +29,30 @@ def get_help(message):
 @bot.message_handler(commands=["weather"])
 def weather_command(message):
     bot.send_message(message.chat.id, "Напиши мне название города и я пришлю сводку погоды")
+
+
+@bot.message_handler(content_types=['text'])
+def weather(message):
+
+    try:
+        # получаем город из сообщения пользователя
+        city = message.text.lower()
+      # формируем запрос
+        url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+f'&units=metric&lang=ru&appid={weather_token}'
+      # отправляем запрос на сервер и сразу получаем результат
+        weather_data = requests.get(url).json()
+      # получаем данные о температуре и о том, как она ощущается
+        temperature = round(weather_data['main']['temp'])
+        temperature_feels = round(weather_data['main']['feels_like'])
+      # формируем ответы
+        w_now = 'Сейчас в городе ' + city + ' ' + str(temperature) + ' °C'
+        w_feels = 'Ощущается как ' + str(temperature_feels) + ' °C'
+      # отправляем значения пользователю
+        bot.send_message(message.from_user.id, w_now)
+        bot.send_message(message.from_user.id, w_feels)
+
+    except Exception as ex:
+        print(ex)
 
 
 @bot.message_handler()
