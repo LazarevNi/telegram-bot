8f91f75db7d715cbcb5799a90b4c5cc63a9b08e9
Nikita Lazarev <Ni.Lazarev@ase-ec.ru>
2024-08-03 14:04:26 +0300

    обработка других сообщений, погода

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import telebot
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-bot_token = os.getenv("TOKEN")
+bot_token = os.getenv("TOKEN_TG")
+weather_token = os.getenv("TOKEN_WEATHER")
 
 if bot_token is None:
     raise Exception("Bot token is not defined")
@@ -14,13 +16,19 @@ bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Я бот для управления чатом. Напиши /help, "
-                              f"чтобы узнать, что я умею.")
+    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Я бот для управления чатом. "
+                                      f"Напиши /help, чтобы узнать, что я умею.")
 
 
 @bot.message_handler(commands=['help'])
 def get_help(message):
-    bot.send_message(message.chat.id, "/start - Запуск бота\n/id - узнать id своего user'а")
+    bot.send_message(message.chat.id, "/start - Запуск бота\n/help - помощь\n/id - узнать id своего user'а\n"
+                                      "/weather - узнать погоду")
+
+
+@bot.message_handler(commands=["weather"])
+def weather_command(message):
+    bot.send_message(message.chat.id, "Напиши мне название города и я пришлю сводку погоды")
 
 
 @bot.message_handler()
@@ -31,6 +39,8 @@ def info(message):
     elif message.text.lower() == '/id':
         user_id = message.from_user.id
         bot.reply_to(message, user_id)
+    else:
+        bot.send_message(message.chat.id, "Не знаю такой команды")
 
 
 bot.polling(non_stop=True)
